f152c27537751d3eac9859eedb94fd947a610693
pardini <pardini@92614e66-937d-4085-8ab4-16f9ba51f51a>
2012-09-15 22:33:15 +0000

    More beginnings...

---
+++ src/main/java/net/pardini/parser/tor/TorBlogParser.java
@@ -0,0 +1,17 @@
+package net.pardini.parser.tor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pardini
+ * Date: 15/09/12
+ * Time: 19:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class TorBlogParser {
+
+    public void parseIndex() {
+
+    }
+
+
+}
